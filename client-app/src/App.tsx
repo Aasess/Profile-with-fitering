621f510e8fba053925aae0c59c03b00a6d3d1a9f
Ashish Neupane <ashishneupane1997@gmail.com>
2022-04-01 01:18:12 +0545

    first text displayed on profile component with scss functionality

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+
+//SCSS
 import "scss/styles.scss";
+
+//COMPONENTS
+import Profile from "scenes/profile";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <Profile />
+    </div>
+  );
 }
 
 export default App;
---
+++ client-app/src/scenes/profile/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      Profile Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+      error placeat at sint accusamus culpa aut quod dolorem. Maxime ipsam
+      itaque fugiat voluptate vitae officia dicta vero blanditiis sunt
+      praesentium. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Possimus expedita aliquid adipisci blanditiis, dolorum, voluptates
+      temporibus, doloribus sapiente vel facilis officia deserunt voluptas
+      perferendis pariatur fugit. Maxime, doloribus blanditiis. Repudiandae? Ad
+      sequi error fugiat voluptatibus repellat, dicta ipsam, cum sed excepturi
+      quis adipisci obcaecati doloremque veniam at rerum optio molestiae qui id
+      eum cupiditate tenetur distinctio? Alias, ipsa? Blanditiis, nesciunt?
+      Obcaecati sed sunt dignissimos quos officia nobis quasi corrupti
+      laudantium, similique libero quibusdam totam optio earum praesentium
+      tempore vitae, labore fuga consectetur qui amet consequatur omnis
+      cupiditate sequi. Maiores, odit? Odit ratione provident cupiditate quas
+      repellat sequi molestias explicabo laboriosam debitis expedita, natus
+      omnis nihil ab odio eos est eveniet sit deleniti aspernatur necessitatibus
+      at cumque optio. Magnam, doloribus cum? Molestias voluptatum nulla
+      repellat nesciunt beatae pariatur totam mollitia, est consectetur et
+      tempora sapiente, perferendis rem libero soluta corporis maiores ab
+      tempore quos nisi ipsam facilis itaque ea. Quod, nostrum.
+    </div>
+  );
+};
+
+export default Profile;
