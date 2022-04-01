e2899a2f51b619247eae389fe674415345a13e0c
Ashish Neupane <ashishneupane1997@gmail.com>
2022-04-01 12:42:13 +0545

    stict null checks set to false due to typo error on react 18 document type

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 
-const container: HTMLElement | any = document.getElementById("root");
+const container: HTMLElement | null = document.getElementById("root");
 // Create a root.
 const root = ReactDOMClient.createRoot(container);
 root.render(<App />);
